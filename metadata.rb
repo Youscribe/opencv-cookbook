c0318fc1f7b7d5d7fd200f1e03afe41644cad3e9
Guilhem Lettron <guilhem@lettron.fr>
2013-04-17 16:46:27 +0200

    release v0.0.3

--- metadata.rb
+++ metadata.rb
@@ -5,7 +5,7 @@ license          "Apache 2.0"
 description      "Install opencv"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 
-version          "0.0.1"
+version          "0.0.3"
 
 depends          "apt"
 depends          "chocolatey"
