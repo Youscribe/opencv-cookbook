325764cdf5e1acd3f2c7076b88758f25f723e75b
Yann <me.show@gmail.com>
2014-02-11 15:36:31 +0100

    start install source cookbook

---
+++ attributes/source.rb
@@ -0,0 +1,3 @@
+default['opencv']['source']['url'] = "http://kent.dl.sourceforge.net/project/opencvlibrary/opencv-unix/2.4.8/opencv-2.4.8.zip"
+default['opencv']['source']['checksum'] = "50cc1433b3654074206f5b3dbfd49848"
+default['opencv']['source']['force_recompile'] = false
---
+++ recipes/install_source.rb
@@ -0,0 +1,47 @@
+#
+# Cookbook Name:: opencv
+# Recipe:: install
+# Author:: Yann Robin <yann.robin@youscribe.com>
+#
+# Copyright 2014, Societe YouScribe.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+include_recipe "build-essential"
+
+remote_file node['opencv']['source']['url'] do
+  path src_filepath
+  checksum node['opencv']['source']['checksum']
+  source node['opencv']['source']['url']
+  backup false
+end
+
+src_filepath  = "#{Chef::Config['file_cache_path'] || '/tmp'}/#{::File.basename(node['opencv']['source']['url'])}"
+
+bash "compile_opencv_source" do
+  cwd ::File.dirname(src_filepath)
+  code <<-EOH
+    unzip #{::File.basename(src_filepath)} -d .
+    cd #{::File.basename(src_filepath, ::File.extname(src_filepath))} &&
+	mkdir release &&
+	cd release &&
+	sudo cmake -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_INSTALL_PREFIX=/usr/local .. &&
+	sudo make -j &&
+	sudo make install
+  EOH
+
+  not_if do 
+    node['opencv']['source']['force_recompile'] == false && ::File.directory?(::File.dirname(src_filepath) + '/' + ::File.basename(src_filepath, ::File.extname(src_filepath)))
+  end
+end
